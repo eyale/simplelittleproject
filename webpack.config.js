0f3abde440f91579565bb916952692554baeb481
Anton Honcharov <4eyalush@gmail.com>
2016-12-04 21:26:51 +0200

    setting up the app

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
+                loaders: ['react-hot', 'babel-loader?presets[]=react,presets[]=es2015,presets[]=stage-0']
             },
             {
                 test: /\.scss$/,
